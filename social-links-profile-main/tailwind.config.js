cff23f505718e27e12a1746cf4f214aa418aaa97
movlan <bmovlanov@gmail.com>
2024-08-01 22:38:33 -0400

    add files

---
+++ social-links-profile-main/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        green: "hsl(75, 94%, 57%)",
+        white: "hsl(0, 0%, 100%)",
+        gray: {
+          700: "hsl(0, 0%, 20%)",
+          800: "hsl(0, 0%, 12%)",
+          900: "hsl(0, 0%, 8%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
